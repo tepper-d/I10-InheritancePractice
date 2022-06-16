45ef22149a47f7afa18696a792046f739e07124e
tepper-d <104649969+tepper-d@users.noreply.github.com>
2022-06-16 15:41:01 -0700

    Add project files.

---
+++ InheritancePractice/Person.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*********************************************************
+ * CIS 123: Introduction to Object-Oriented Programming
+ * Module 10: Inheritance Assignment (Individual)
+ * Dominique Tepper, 16JUN2022
+ * 
+ * 1. Complete the base and derived classes:
+ *      a. Person       base
+ *      b. Student      derived
+ *      c. Faculty      derived
+ *      d. Admin        derived
+ *      e. Staff        derived
+ * ******************************************************/
+
+namespace InheritancePractice
+{
+/* ******************************************************
+ * 1-A. Person class properties
+ *          firstName   string
+ *          lastName    string
+ *          email       string    
+ *      Method
+ *      a. PersonWrite
+ * ********************************* Tepper, 06JUN2022 */
+    public class Person
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Person person &&
+               firstName == person.firstName &&
+               lastName == person.lastName &&
+               email == person.email;
+        }
+
+        public override int GetHashCode()
+        {
+            return Tuple.Create(firstName, lastName, email).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"Person(" +
+                $"FirstName='{firstName}', " +
+                $"LastName='{lastName}',  " +
+                $"Email={email})";
+        }
+
+        public void SayHello()
+        {
+            Console.WriteLine($"Hi, I'm {firstName} {lastName}!");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var person1 = new Person { Name = "Jan", Age = 21 };
+            var person2 = new Person { Name = "Jan", Age = 21 };
+
+            Console.WriteLine(person1.GetHashCode());
+            Console.WriteLine(person2.GetHashCode());
+            Console.WriteLine(person1.Equals(person2));
+            Console.WriteLine(person1.ToString());
+            Console.WriteLine(person2.ToString());
+        }
+    }
+}
